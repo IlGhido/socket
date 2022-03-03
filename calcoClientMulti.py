0061c0954d12cbdc92020e690891cd1ea79d4306
IlGhido <nicolo.ghidoli@gmail.com>
2022-03-03 12:19:01 +0100

    calcoClientMulti 2.0 commentato

--- calcoClientMulti.py
+++ calcoClientMulti.py
@@ -16,47 +16,52 @@ NUM_WORKERS=2
 def genera_richieste(num,address,port):
     start_time_thread= time.time()
     try:
-        s=socket.socket()
-        s.connect((address,port))
+        s=socket.socket() #creo il socket
+        s.connect((address,port)) #mi connetto al server
         print(f"\n{threading.current_thread().name} {num+1}) Connessione al server: {address}:{port}")
-    except:
+    except: #se ricevo errori invio un avviso e chiudo l'esecuzione
         print(f"{threading.current_thread().name} Qualcosa è andato storto, sto uscendo... \n")
         sys.exit()
     #1. rimpiazzare questa parte con la generazione di operazioni e numeri random, non vogliamo inviare sempre 3+5
-    primoNumero=random.randint(0,100)
-    secondoNumero=random.randint(0,100)
-    numeroOperazione=random.randint(0,4)
-    operazioni=["+","-","*","/","%"]
-    operazione=operazioni[numeroOperazione]
-    # if(numeroOperazione==1):
-    #     operazione="+"
-    # elif(numeroOperazione==2):
-    #     operazione="-"
-    # elif(numeroOperazione==3):
-    #     operazione="*"
-    # elif(numeroOperazione==4):
-    #     operazione="/"
-    # else:
-    #     operazione="%"
+    primoNumero=random.randint(0,100) #numero casuale da 1 a 100
+    secondoNumero=random.randint(0,100) #numero casuale da 1 a 100
+    numeroOperazione=random.randint(0,4) #numero casuale da 0 a 4
+    operazioni=["+","-","*","/","%"] #lista di operazioni
+    operazione=operazioni[numeroOperazione] #utilizzo il numero casuale (usandolo come indice) per scegliere un'operazione
+    '''
+    metodo alternativo per scegliere l'operazione:
+    if(numeroOperazione==1):
+        operazione="+"
+    elif(numeroOperazione==2):
+        operazione="-"
+    elif(numeroOperazione==3):
+        operazione="*"
+    elif(numeroOperazione==4):
+        operazione="/"
+    else:
+        operazione="%"'''
     #primoNumero=3
     #operazione="+"
     #secondoNumero=5
 
     #2. comporre il messaggio, inviarlo come json e ricevere il risultato
+
+    #compongo il messaggio
     messaggio={
-            'primoNumero':primoNumero, 
-            'operazione':operazione, 
-            'secondoNumero':secondoNumero
-        }
+        'primoNumero':primoNumero, 
+        'operazione':operazione, 
+        'secondoNumero':secondoNumero
+    }
 
-    sock_service = socket.socket()
-    sock_service.connect((address, port))
-    messaggio=json.dumps(messaggio) #trasforma l'oggetto in una stringa
+    #lo invio come json
+    messaggio=json.dumps(messaggio)
     print("Invio richiesta:", messaggio)
-    sock_service.sendall(messaggio.encode("UTF-8"))
-    data=sock_service.recv(1024)
+    s.sendall(messaggio.encode("UTF-8"))
 
-    if not data:
+    #ricevoi il risultato
+    data=s.recv(1024)
+
+    if not data: #se come risultato non riceviamo alcuno dato
         print(f"{threading.current_thread().name}: Server non risponde. Exit")
     else:
         print(f"{threading.current_thread().name}: Risultato: {data.decode()}") # trasforma il vettore di byte in stringa
@@ -65,18 +70,20 @@ def genera_richieste(num,address,port):
     print(f"{threading.current_thread().name} tempo di esecuzione time=", end_time_thread-start_time_thread)
 
 if __name__ == '__main__':
+    #esecuzione seriale delle operazioni
     start_time=time.time()
     # 3 ciclo per chiamare NUM_WORKERS volte la funzione genera richieste alla quale passo i parametri (num,SERVER_ADDRESS, SERVER_PORT)
     for num in range(NUM_WORKERS):
         genera_richieste(num, SERVER_ADDRESS, SERVER_PORT)
     end_time=time.time()
     print("Total SERIAL time=", end_time - start_time)
-     
+    
+    #esecuzione con i thread delle operazioni
     start_time=time.time()
     threads=[]
     # 4 ciclo per chiamare NUM_WORKERS volte la funzione genera richieste tramite l'avvio di un thread al quale passo i parametri args=(num,SERVER_ADDRESS, SERVER_PORT,)
-    for num in range(NUM_WORKERS):
-        thread = threading.Thread(target=genera_richieste, args=(num, SERVER_ADDRESS, SERVER_PORT))
+    for num in range(NUM_WORKERS): #NUM_WORKERS volte
+        thread = threading.Thread(target=genera_richieste, args=(num, SERVER_ADDRESS, SERVER_PORT)) #creo il thread che svolge genera_richieste, con come argomenti args
         # ad ogni iterazione appendo il thread creato alla lista threads
         threads.append(thread)
     # 5 avvio tutti i thread
@@ -88,11 +95,12 @@ if __name__ == '__main__':
     end_time=time.time()
     print("Total THREADS time= ", end_time - start_time)
 
+    #esecuzione con i process delle operazioni
     start_time=time.time()
     processes=[]
     # 7 ciclo per chiamare NUM_WORKERS volte la funzione genera richieste tramite l'avvio di un processo al quale passo i parametri args=(num,SERVER_ADDRESS, SERVER_PORT,)
-    for num in range(NUM_WORKERS):
-        process = multiprocessing.Process(target=genera_richieste, args=(num, SERVER_ADDRESS, SERVER_PORT))
+    for num in range(NUM_WORKERS): #NUM_WORKERS volte
+        process = multiprocessing.Process(target=genera_richieste, args=(num, SERVER_ADDRESS, SERVER_PORT)) #creo il thread che svolge genera_richieste, con come argomenti args
         # ad ogni iterazione appendo il thread creato alla lista threads
         processes.append(process)
     # 8 avvio tutti i processi
